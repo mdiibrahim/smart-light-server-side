401e99110a0b9f1ecf791eecec2bf407f998d283
mdiibrahim <mdiibrahim549@gmail.com>
2023-04-08 16:24:59 +0600

    turn led and historu done

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { SerialPort } = require('serialport');
+require('dotenv').config();
+const { ReadlineParser } = require('@serialport/parser-readline')
+const portNo = process.env.PORT || 5000;
+
+// Express middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@smart-home.a4v8nby.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Serial communication with Arduino
+const port = new SerialPort({
+    path: 'COM4',
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+parser.on('data', data => console.log('Received data from Arduino:', data));
+
+// MongoDB collection for LED state and timestamp
+const collection = client.db('Smart-Home').collection('ledstate');
+
+// Express routes
+app.post('/led/on', async (req, res) => {
+    port.write('1');
+    const ledState = req.body;
+    const result = await collection.insertOne(ledState)
+    res.send(result)
+});
+
+app.post('/led/off', async (req, res) => {
+    port.write('0');
+    const ledState = req.body;
+    const result = await collection.insertOne(ledState)
+    res.send(result)
+
+});
+
+app.get('/led/history', async (req, res) => {
+    const ledStates = await collection.find({}).toArray();
+    res.send(ledStates);
+});
+app.delete('/history/delete', async (req, res) => {
+  
+    const result = await collection.deleteMany({});
+    res.send(result);
+
+})
+
+app.get('/', async (req, res) => {
+    res.send('Smart Home server is running');
+});
+
+app.listen(portNo, () => console.log(`Smart Home running on ${portNo}`));
